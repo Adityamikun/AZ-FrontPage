d7e8aef905c120cdd0117f2cd999dadfc4a378d1
Adityamikun <narayanadityadr42@gmail.com>
2023-06-29 19:53:05 +0530

    Initial commit

---
+++ src/pages/algo-zenith-l-p.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import "./algo-zenith-l-p.css";
+const AlgoZenithLP = () => {
+  return (
+    <div className="algozenith-lp">
+      <div className="hero">
+        <img className="laptop-bg" alt="" src="/laptop--bg@2x.png" />
+        <div className="frame-parent">
+          <div className="rectangle-parent">
+            <div className="frame-child" />
+            <div className="frame-item" />
+            <div className="lorem-ipsum-dolor">Lorem ipsum dolor sit amet</div>
+            <div className="lorem-ipsum-dolor1">Lorem ipsum dolor sit amet</div>
+          </div>
+          <div className="rectangle-parent">
+            <div className="frame-child" />
+            <div className="frame-item" />
+            <div className="lorem-ipsum-dolor">Lorem ipsum dolor sit amet</div>
+            <div className="lorem-ipsum-dolor1">Lorem ipsum dolor sit amet</div>
+          </div>
+        </div>
+        <div className="above-laptop">
+          <div className="be-better-at">{`Be better at DSA & CP`}</div>
+          <div className="lorem-ipsum-dolor4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </div>
+          <div className="frame-group">
+            <button className="frame-wrapper">
+              <div className="join-now-wrapper">
+                <div className="join-now-container">
+                  <b className="join-now">{`Join now `}</b>
+                  <b className="b">{`->`}</b>
+                </div>
+              </div>
+            </button>
+            <button className="view-curriculum-wrapper">
+              <b className="view-curriculum">View curriculum</b>
+            </button>
+          </div>
+        </div>
+        <div className="frame-container">
+          <div className="mask-group-parent">
+            <div className="mask-group">
+              <img
+                className="m10i307n008fc05-2-icon"
+                alt=""
+                src="/1601m10i307n008fc05-2@2x.png"
+              />
+              <img
+                className="m10i307n008fc05-1-icon"
+                alt=""
+                src="/1601m10i307n008fc05-1@2x.png"
+              />
+              <div className="frame-div">
+                <div className="frame-parent1">
+                  <img className="frame-icon" alt="" src="/frame-218.svg" />
+                  <b className="trees">Trees</b>
+                </div>
+                <div className="frame-parent2">
+                  <img className="frame-icon" alt="" src="/frame-221.svg" />
+                  <b className="trees">Graphs</b>
+                </div>
+                <div className="group-parent">
+                  <img className="group-icon" alt="" src="/group-222.svg" />
+                  <b className="trees">Time Comlexity</b>
+                </div>
+                <div className="frame-parent3">
+                  <img className="frame-icon" alt="" src="/frame-220.svg" />
+                  <b className="trees">Dynamic Programming</b>
+                </div>
+                <div className="group-container">
+                  <img className="group-icon" alt="" src="/group-217.svg" />
+                  <b className="trees">Backtracking</b>
+                </div>
+                <div className="frame-parent4">
+                  <img className="frame-icon" alt="" src="/frame-215.svg" />
+                  <b className="trees">Binary Search</b>
+                </div>
+                <div className="frame-parent5">
+                  <img className="frame-icon" alt="" src="/frame-216.svg" />
+                  <b className="trees">{`Branch & Bound`}</b>
+                </div>
+                <div className="frame-parent6">
+                  <img className="frame-icon" alt="" src="/frame-224.svg" />
+                  <b className="trees">{`Branch & Bound`}</b>
+                </div>
+                <div className="frame-parent7">
+                  <img className="frame-icon" alt="" src="/frame-219.svg" />
+                  <b className="trees">Two pointers</b>
+                </div>
+              </div>
+            </div>
+            <img className="frame-child8" alt="" src="/group-319.svg" />
+            <div className="ellipse-div" />
+            <img className="frame-child9" alt="" src="/group-270@2x.png" />
+          </div>
+          <div className="frame-child10" />
+        </div>
+      </div>
+      <div className="graph-parent">
+        <img className="graph-icon" alt="" src="/graph.svg" />
+        <div className="invest-in-skills-earn-10x-of-parent">
+          <div className="invest-in-skills-container">
+            <p className="invest-in-skills">Invest in skills,</p>
+            <p className="invest-in-skills">
+              <span className="earn">earn</span>
+              <span className="span">{` `}</span>
+              <span className="earn">10X</span>
+              <span> of what you paid.</span>
+            </p>
+          </div>
+          <div className="lorem-ipsum-dolor5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+          <div className="frame-parent8">
+            <div className="rectangle-container">
+              <div className="frame-child11" />
+              <div className="cryear-highest-fresher-container">
+                <p className="invest-in-skills">
+                  <b>1.2 Cr/Year</b>
+                </p>
+                <p className="highest-fresher-package">
+                  highest fresher package
+                </p>
+              </div>
+            </div>
+            <div className="rectangle-parent1">
+              <div className="frame-child11" />
+              <div className="cryear-highest-fresher-container">
+                <p className="invest-in-skills">
+                  <b>{`Multiple <100 Ranks`}</b>
+                </p>
+                <p className="highest-fresher-package">
+                  in kickstart last year
+                </p>
+              </div>
+            </div>
+            <div className="rectangle-parent2">
+              <div className="frame-child11" />
+              <div className="cryear-highest-fresher-container">
+                <p className="invest-in-skills">
+                  <b>1000+ Offers</b>
+                </p>
+                <p className="highest-fresher-package">from top companies</p>
+              </div>
+            </div>
+            <div className="rectangle-parent3">
+              <div className="frame-child11" />
+              <div className="cryear-highest-fresher-container">
+                <p className="invest-in-skills">
+                  <b>Trusted by IITians</b>
+                </p>
+                <p className="highest-fresher-package">for their career prep</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mask-group-group">
+          <img className="mask-group-icon" alt="" src="/mask-group@2x.png" />
+          <div className="frame-parent9">
+            <div className="parent">
+              <div className="div">₹13,999</div>
+              <b className="b1">₹19,999</b>
+              <div className="premium-victory-batch">Premium Victory Batch</div>
+            </div>
+            <img className="frame-child15" alt="" src="/frame-2151.svg" />
+          </div>
+          <div className="frame-parent10">
+            <div className="frame-parent11">
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">100+ Hrs Live Content</div>
+              </div>
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">50+ Hrs Contest</div>
+              </div>
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">500+ Problems</div>
+              </div>
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">
+                  Includes an interview bootcamp and access to a mentor network
+                </div>
+              </div>
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">
+                  Enjoy access to our content for one year (paid extensions
+                  available)
+                </div>
+              </div>
+              <div className="rectangle-parent4">
+                <div className="frame-child16" />
+                <div className="hrs-live-content">
+                  Flexible payment options, including no-cost EMI, are
+                  available.
+                </div>
+              </div>
+            </div>
+            <div className="ctas">
+              <div className="join-now-frame">
+                <div className="join-now-container1">
+                  <b>{`Join Now `}</b>
+                  <b className="b">{`->`}</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="algozenith-lp-child" />
+    </div>
+  );
+};
+
+export default AlgoZenithLP;
